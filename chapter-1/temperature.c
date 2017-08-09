b6b1a0f479c3c1cde36cff518577a25169eebf12
Jorja Hung <jorjahung@gmail.com>
2017-08-09 14:53:36 +0100

    Temperature exercise (1.2)

---
+++ chapter-1/temperature.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20, ..., 300; floating-point version */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr−32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
+/*
+%d		print as decimal integer
+%6d		print as decimal integer, at least 6 characters wide
+%f		print as floating point
+%6f		print as floating point, at least 6 characters wide
+%.2f		print as floating point, 2 characters after decimal point
+%6.2f	print as floating point, at least 6 wide and 2 after decimal point
+*/
